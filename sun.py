85f30605e13197ee033345ce77b13db55b77f56a
Rodney Peck <rodney@fogcat5.com>
2025-01-07 23:49:57 -0800

    CC-7 log as collection and scheduling runs

--- sun.py
+++ sun.py
@@ -19,7 +19,8 @@ def run_script(run_time):
     command = f"/usr/bin/echo '(/app/gcloud_upload.sh 2>&1 </dev/null) >> /var/log/video' | /usr/bin/at {local_run_time.strftime('%I:%M %p')}"
     print(command)
     with open("/var/log/camera-collector", "a") as f:
-        f.write(f"{str(datetime.now())} {command}\n")
+        t=datetime.now(pytz.timezone(os.getenv("TZ", "America/Los_Angeles")))
+        f.write(f"{t} {command}\n")
     os.system(command)
 
 location = LocationInfo("Seacliff", "USA", "America/Los_Angeles", 36.9741, -121.9158)
